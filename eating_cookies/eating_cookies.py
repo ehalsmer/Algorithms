0d82c5a45c2301a0d9b9b2b403ef113b6311c26f
Eleasah Halsmer <eleasahjh@gmail.com>
2019-12-12 14:53:21 -0700

    eating_cookies cached as object

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -6,19 +6,26 @@ import sys
 # a solution that is more efficient than the naive 
 # recursive solution
 
-# Tribonacci numbers, see https://oeis.org/A000073
-def eating_cookies(n, cache=None):
-  memo = {0:1, 1:1, 2:2}
-  if n in memo:
-    print('found in memo')
-    return memo[n]
+def small_eating_cookies(n):
+  if n <= 1:
+    return 1
+  elif n == 2:
+    return 2
   else:
-    computed = eating_cookies(n-3) + eating_cookies(n-2) + eating_cookies(n-1)
-    memo[n] = computed
-    print('computed')
-    return memo[n]
+    return small_eating_cookies(n-3) + small_eating_cookies(n-2) + small_eating_cookies(n-1)
 
-print(eating_cookies(3))
+
+# Tribonacci numbers, see https://oeis.org/A000073
+def eating_cookies(n):
+  cache = {0:1, 1:1, 2:2, 3:4}
+  def inner(n):
+    nonlocal cache
+    if n not in cache:
+      cache[n] = inner(n-3) + inner(n-2) + inner(n-1)
+    return cache[n]
+  return inner(n)
+
+# print('15: ',eating_cookies(15))
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
