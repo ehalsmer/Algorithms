1ced6825c818fe6703305c2c8d07fbd53c16c8bb
Eleasah Halsmer <eleasahjh@gmail.com>
2019-12-12 14:03:53 -0700

    wip caching for eating_cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -8,12 +8,17 @@ import sys
 
 # Tribonacci numbers, see https://oeis.org/A000073
 def eating_cookies(n, cache=None):
-  if n <= 1:
-    return 1
-  elif n == 2:
-    return 2
+  memo = {0:1, 1:1, 2:2}
+  if n in memo:
+    print('found in memo')
+    return memo[n]
   else:
-    return eating_cookies(n-3) + eating_cookies(n-2) + eating_cookies(n-1)
+    computed = eating_cookies(n-3) + eating_cookies(n-2) + eating_cookies(n-1)
+    memo[n] = computed
+    print('computed')
+    return memo[n]
+
+print(eating_cookies(3))
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
